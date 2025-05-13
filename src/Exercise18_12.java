083bfb508a4eb8a78929ef10759be69b0d0c23f8
xmen2100 <119138813+xmen2100@users.noreply.github.com>
2025-05-13 10:20:44 -0700

    18.12

--- src/Exercise18_12.java
+++ src/Exercise18_12.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Exercise18_12 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter a string: ");
+        reverseDisplay(input.next());
+    }
+
+    public static void reverseDisplay(String value) {
+        reverseDisplay(value, value.length() - 1);
     }
 
     public static void reverseDisplay(String value, int high) {
+        if (!value.isEmpty()) {
+            System.out.print(value.charAt(high));
 
+            reverseDisplay(value.substring(0, high));
+        }
     }
 
 }
