4be30e653b4791239455e1727772310bc342e8d9
xmen2100 <119138813+xmen2100@users.noreply.github.com>
2025-05-12 19:02:50 -0700

    Exercise 18.5 and 18.6

---
+++ src/Exercise18_05.java
@@ -0,0 +1,19 @@
+public class Exercise18_05 {
+    public static void main(String[] args) {
+        System.out.printf("%-10s%s\n", "i", "m(i)");
+        System.out.println("------------------");
+
+        for (int i = 1; i < 11; i++) {
+            System.out.printf("%-10d%f\n", i, m(i));
+        }
+    }
+
+    public static double m(int i) {
+        if (i == 1) {
+            return 1.0 / 3;
+        }
+        else {
+            return m(i - 1) + i / (2 * i + 1.0);
+        }
+    }
+}
