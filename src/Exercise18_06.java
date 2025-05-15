798372b0ddc730746203baff149233dde7d679f3
xmen2100 <119138813+xmen2100@users.noreply.github.com>
2025-05-14 18:11:00 -0700

    no hyphens

--- src/Exercise18_06.java
+++ src/Exercise18_06.java
@@ -1,7 +1,6 @@
 public class Exercise18_06 {
     public static void main(String[] args) {
         System.out.printf("%-10s%s\n", "i", "m(i)");
-        System.out.println("------------------");
 
         for (int i = 1; i < 11; i++) {
             System.out.printf("%-10d%f\n", i, m(i));
--- src/Test3.java
+++
@@ -1,6 +0,0 @@
-public class Test3 {
-    public static void main(String[] args) {
-        String s = "w";
-        System.out.println(s.substring(1));
-    }
-}
